e788b967d758d5f519c27bb8f2e93d9e2aa305a6
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-05 14:12:31 -0400

    Implement board, selection, and drop_piece

--- connect.py
+++ connect.py
@@ -9,14 +9,29 @@ def make_selection(player):
     try:
         if 0 <= int(selection) <= 6:
             selection = int(selection)
+            return selection
         else:
             raise Exception
     except:
         print("Invalid selection. Try again.")
         return None
-    return selection
 
-board = create_board
+def drop_piece(col, is_player_one, board):
+    try:
+        drop_piece_helper(col, is_player_one, board)
+        return True
+    except:
+        print("No more room in that column. Try again.")
+        return False
+
+def drop_piece_helper(col, is_player_one, board):
+    for i in range(len(board)):
+        if board[-1-i][col] == 0:
+            board[-1-i][col] = 1 if is_player_one else 2
+            return
+    raise Exception
+
+board = create_board()
 game_over = False
 is_player_one = True
 
@@ -25,10 +40,15 @@ while not game_over:
         selection = make_selection(1)
         if selection is None:
             continue
+        d = drop_piece(selection, is_player_one, board)
+        if not d:
+            continue
     else:
         selection = make_selection(2)
         if selection is None:
             continue
+        d = drop_piece(selection, is_player_one, board)
+        if not d:
+            continue
+    print(board)
     is_player_one = not is_player_one
-
-
